1841f6d1158361acedb9fdba73d5e6dcb45a4d17
Lenny8ooPenny <71707583+Lenny8ooPenny@users.noreply.github.com>
2020-09-22 08:49:50 -0400

    Update CreateNewDistributionGroup.ps1

--- CreateNewDistributionGroup.ps1
+++ CreateNewDistributionGroup.ps1
@@ -1,9 +1,9 @@
 #CreateNewDistributionGroup
-#and adds members to it 
+    #and adds members to it 
     #Author: Lenny K
     #Date: 9/21/20
     
 $GroupName = Read-Host -Object 'Enter name of the group'
 $Credential = Get-Credential ''
-New-ADGroup -Name $GroupName -GroupScope Global -Credential $Credential -GroupCategory Distribution -Path "OU=Distribution,OU=Groups,OU=Mill Steel,DC=Millsteel,DC=Local"
+New-ADGroup -Name $GroupName -GroupScope Global -Credential $Credential -GroupCategory Distribution -Path "OU=,OU=,OU=,DC=,DC="
 Add-ADGroupMember -Identity "$GroupName" -Credential $Credential
