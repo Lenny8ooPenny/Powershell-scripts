509bacbc15872809c8da2f3d5cefba0a65deb160
Lenny8ooPenny <71707583+Lenny8ooPenny@users.noreply.github.com>
2020-09-22 08:51:07 -0400

    Update CreateNewSecurityGroup.ps1

--- CreateNewSecurityGroup.ps1
+++ CreateNewSecurityGroup.ps1
@@ -1,10 +1,10 @@
 #CreateNewSecurityGroup
-#and adds members to it 
+    #and adds members to it 
     #Author: Lenny K
     #Date: 9/21/20
     
     $GroupName = Read-Host -Object 'Enter name of the group'
     $Credential = Get-Credential ''
     New-ADGroup -Name $GroupName -GroupScope Global -Credential $Credential 
-    -GroupCategory Security -Path "OU=Security,OU=Groups,OU=Mill Steel,DC=Millsteel,DC=Local"
+    -GroupCategory Security -Path "OU=,OU=,OU=,DC=,DC="
     Add-ADGroupMember -Identity "$GroupName" -Credential $Credential
